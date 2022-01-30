f958f666d84458d0b96062603a5869feee70096a
event298horizon <awasthisaurabh0700@gmail.com>
2022-01-30 17:14:20 +0530

    Database and error Msg
    Data base connected successfully and saved.

--- Code/model/User.js
+++ Code/model/User.js
@@ -1,14 +1,10 @@
 const mongoose = require("mongoose");
-const { date } = require("joi");
+// const { date } = require("joi");
 const userSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
-  },
-  createdAt: {
-    type: date,
-    default: Date.now(),
-  },
+  }
 });
 
-module.exports = mongoose.model(userSchema, "User");
+module.exports = mongoose.model("users", userSchema);
